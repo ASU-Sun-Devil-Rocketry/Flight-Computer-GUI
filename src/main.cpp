6cecbbd2dfa0cdc9c25602690d96a749d9ca9572
Colton Acosta <cacost12@asu.edu>
2021-09-18 19:12:42 -0700

    Created main source code

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/************************************************************* 
+* Flight Computer GUI -- Program to interface with the       *
+*                        embedded flight computer and        *
+*                        provide flight visualization        *
+*                                                            * 
+* Contributers: Colton Acosta, Caden Kriese, Dylan Larson    *
+*                                                            * 
+* Date: 9/18/2021                                            *
+*************************************************************/ 
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+int main(void){
+
+    return(0);
+}
