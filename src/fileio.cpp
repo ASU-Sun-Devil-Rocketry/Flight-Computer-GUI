bbd0bd33c4905d1b78d1c926908ed230f626ea58
Andrew Byers <bassbolero@gmail.com>
2021-09-21 14:59:27 -0700

    Created fileio.cpp

--- src/fileio.cpp
+++ src/fileio.cpp
@@ -1,15 +1,58 @@
+/************************************************************* 
+* fileio.cpp - Program designed to read data from the rocket *
+*              and output to a text file named by the user   *
+*                                                            *
+* Contributers: Andrew Byers                                 *
+*                                                            * 
+* Date last updated: 9/20/2021                               *
+*************************************************************/ 
 #include <iostream>
 #include <fstream>
+#include <cstring>
+#include <cctype>
 using namespace std;
 
+//FUNCTION: Reads text data from a dummy text file (dataInput.txt) and writes it 
+//          to a new file of the name given in the parameter
 void writeToFile(string fileName){
+    fileName = fileName + ".txt";
     ofstream dataFile(fileName);
-    dataFile << "This file was written to correctly!";
-    cout << "The file has finished writing" << endl;
+    string textTest;
+    ifstream readFile("dummyFile.txt");
+    while (getline(readFile, textTest)){
+        dataFile << textTest << endl;
+    }
+    cout << "The file has finished writing to " << fileName << endl;
+    readFile.close();
+    dataFile.close();
 }
+
+//TODO: Acccount for the following variables, and format properly in final program:
+//      
+//      X,Y,Z Position | Roll, Pitch, and Yaw Angles | X,Y,Z Speeds/Velocities 
+//      Roll, Pitch, and Yaw Rotation Rates/Angular Velocities | Barometric Pressure/Altitude
 int main(){
+    bool validFileName = true;
     string desiredFileName;
-    cout << "Enter a file name: ";
-    cin >> desiredFileName;
+    do {  
+        cout << "Enter a file name: ";
+        getline(cin, desiredFileName);
+        cout << "Read: " << desiredFileName << endl;
+        for(int i = 0; i < sizeof(desiredFileName); i++){
+            if(isspace(desiredFileName[i])){
+                validFileName = false;
+                cout << "Please enter a file name with no spaces" << endl;
+                break;
+            }
+            else if(desiredFileName[i] == '.'){
+                validFileName = false;
+                cout << "Please enter a file name with no extension" << endl;
+                break;
+            }
+            else{
+                validFileName = true;
+            }
+        }
+    } while(!validFileName);
     writeToFile(desiredFileName);
 }
