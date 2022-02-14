320e0c8bd85c469701b86ebd4e98d9681300e0a2
Colton Acosta <cacost12@asu.edu>
2022-02-13 17:48:54 -0700

    changed fragment shader to grey

--- src/openGL/shaders/fragmentShader.glsl
+++ src/openGL/shaders/fragmentShader.glsl
@@ -5,18 +5,17 @@ out vec4 FragColor;
 void main()
 {
     // rgb colors
-    float red = 16;
-    float green = 189;
-    float blue = 13;
-    float alpha = 1;
+    float red = 179;
+    float green = 179;
+    float blue = 179;
+    float alpha = 1.0;
 
     // convert to normalized paramters
     float redn = red/255.0;
     float greenn = green/255.0;
     float bluen = blue/255.0;
-    float alphan = alpha/255.0;
 
     // set color output
-    FragColor = vec4(redn, greenn, bluen, alphan);
+    FragColor = vec4(redn, greenn, bluen, alpha);
 }
 
