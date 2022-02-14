5043d8d356a30531a9925cdd140cae63dad91603
Colton Acosta <cacost12@asu.edu>
2022-02-13 17:44:04 -0700

    rewrote fragmentShader to take non-normalized rgb colors as input

--- src/openGL/shaders/fragmentShader.glsl
+++ src/openGL/shaders/fragmentShader.glsl
@@ -4,6 +4,19 @@ out vec4 FragColor;
 
 void main()
 {
-    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    // rgb colors
+    float red = 16;
+    float green = 189;
+    float blue = 13;
+    float alpha = 1;
+
+    // convert to normalized paramters
+    float redn = red/255.0;
+    float greenn = green/255.0;
+    float bluen = blue/255.0;
+    float alphan = alpha/255.0;
+
+    // set color output
+    FragColor = vec4(redn, greenn, bluen, alphan);
 }
 
